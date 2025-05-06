67689683fb9a324212ccfe1631da597f47b74e99
jay.zbox.linux <jay.zbox.linux.com>
2025-05-07 01:06:58 +0800

    添加飞书和微信的安装

--- src/onekeyinstall/tools/install_feishu.py
+++ src/onekeyinstall/tools/install_feishu.py
@@ -1,6 +1,7 @@
 
 from onekeyinstall.installer import Installer
 from onekeyinstall.registry import register
+from onekeyinstall.utils import Arch, get_offline_install_package_suffix
 
 
 @register("feishu")
@@ -8,5 +9,18 @@ class InstallFeishu(Installer):
 
     PACKAGE_NAME = "feishu"
 
+# 飞书的带有验证
+# https://lf3-ug-sign.feishucdn.com/ee-appcenter/9bcfe8ba/Feishu-linux_x64-7.36.11.deb?lk3s=fb957577&x-expires=1746553895&x-signature=UmNF7PnI%2BqaC6T50KtP6PRp1u84%3D
+# https://lf6-ug-sign.feishucdn.com/ee-appcenter/9bcfe8ba/Feishu-linux_x64-7.36.11.rpm?lk3s=fb957577&x-expires=1746554076&x-signature=PTOibsWhggMg827%2BNstftdxXWXQ%3D
+
+# https://lf6-ug-sign.feishucdn.com/ee-appcenter/484fc204/Feishu-linux_arm64-7.36.11.deb?lk3s=fb957577&x-expires=1746554124&x-signature=Rcr3%2F0QJaLAOnyNqw7IoA%2BGYU1Y%3D
+# https://lf3-ug-sign.feishucdn.com/ee-appcenter/78243739/Feishu-linux_mips64el-7.22.9.deb?lk3s=fb957577&x-expires=1746554155&x-signature=hj%2B%2BoAmBjoT3VcnRnanQeZ9DPuw%3D
+    def pre_install(self):
+        url = None
+
+
+
+
     def install(self):
         return super().install()
+
--- src/onekeyinstall/tools/install_wechat.py
+++ src/onekeyinstall/tools/install_wechat.py
@@ -1,12 +1,37 @@
 
 from onekeyinstall.installer import Installer
 from onekeyinstall.registry import register
+from onekeyinstall.utils import Arch, get_offline_install_package_suffix
 
 
-@register("wechat")
-class InstallFeishu(Installer):
+@register("weichat")
+class InstallWeichat(Installer):
 
-    PACKAGE_NAME = "feishu"
+    PACKAGE_NAME = "weichat"
+
+
+    def pre_install(self):
+        url = None
+        offline_package_suffix = get_offline_install_package_suffix()
+        if self._system.arch == Arch.X86:
+            _arch = "X86_64"
+        elif self._system.arch == Arch.ARM:
+            _arch = "arm64"
+        elif self._system.arch == Arch.LOONGARCH:
+            _arch = "LoongArch"
+        else:
+            return False
+        
+        if "deb" in offline_package_suffix:
+            _suffix = "deb"
+        elif "rpm" in offline_package_suffix:
+            _suffix = "rpm"
+        else:
+            return False
+
+        url = f"https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_{_arch}.{_suffix}"
+        self.offline_package = self.downloader.download(url, f"WeChatLinux.{_suffix}")
+
 
     def install(self):
-        return super().install()
+        self.pm_executor.install(self.offline_package)
